4b77566abd75ecee80e8d3fec93fdf30b44ed71b
Nexentio <nexentiosoft@gmail.com>
2026-08-17 17:10:34 +0300

    fix(treatments): replicate exact General Dentistry components and layout to Cosmetic Dentistry

--- src/components/CosmeticDentistryDetailView.tsx
+++ src/components/CosmeticDentistryDetailView.tsx
@@ -1,25 +1,25 @@
 'use client';
 
 import React from 'react';
-import CosmeticDentistryIntroSection from '@/components/treatment-sections/CosmeticDentistryIntroSection';
-import CosmeticDentistryAccordionSection from '@/components/treatment-sections/CosmeticDentistryAccordionSection';
-import CosmeticDentistryJourneySection from '@/components/treatment-sections/CosmeticDentistryJourneySection';
+import GeneralDentistryIntroSection from '@/components/treatment-sections/GeneralDentistryIntroSection';
+import GeneralDentistryAccordionSection from '@/components/treatment-sections/GeneralDentistryAccordionSection';
+import GeneralDentistryJourneySection from '@/components/treatment-sections/GeneralDentistryJourneySection';
 import TreatmentServicesIncludedSection from '@/components/treatment-sections/TreatmentServicesIncludedSection';
 import RealPatientsSection from '@/components/RealPatientsSection';
 import TreatmentReviewsSection from '@/components/treatment-sections/TreatmentReviewsSection';
-import CosmeticDentistryFaqSection from '@/components/treatment-sections/CosmeticDentistryFaqSection';
+import GeneralDentistryFaqSection from '@/components/treatment-sections/GeneralDentistryFaqSection';
 import TreatmentInteractiveQuoteForm from '@/components/treatment-sections/TreatmentInteractiveQuoteForm';
 
 export default function CosmeticDentistryDetailView() {
   return (
     <>
-      <CosmeticDentistryIntroSection />
-      <CosmeticDentistryAccordionSection />
-      <CosmeticDentistryJourneySection />
+      <GeneralDentistryIntroSection />
+      <GeneralDentistryAccordionSection />
+      <GeneralDentistryJourneySection />
       <TreatmentServicesIncludedSection />
       <RealPatientsSection />
       <TreatmentReviewsSection />
-      <CosmeticDentistryFaqSection />
+      <GeneralDentistryFaqSection />
       <div id="contact">
         <TreatmentInteractiveQuoteForm defaultTreatment="Cosmetic Dentistry" />
       </div>
